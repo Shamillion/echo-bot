84c6c6811463fe596c49644389cf1b2a8dceadec
Shamil <ns-80@yandex.ru>
2021-02-12 21:46:28 +0500

    commit #2

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,76 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, DeriveGeneric #-} 
 module Main where
 
+import System.IO
+import System.IO.Unsafe (unsafePerformIO)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as BC
 import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString.Lazy.Char8 as LC
 import Network.HTTP.Simple
+import Data.Aeson
+import Data.Text as T
+import GHC.Generics
 
+data Chat = Chat {
+                   id :: Int
+                 , first_name :: T.Text
+                 , username :: T.Text 
+                 , typeChat :: T.Text
+                 } deriving Show
+
+instance FromJSON Chat where
+  parseJSON (Object v) =
+    Chat <$> v .: "id" 
+         <*> v .: "first_name"
+         <*> v .: "username"
+         <*> v .: "type"
+ 
+data From = From {
+                   from_id :: Int
+                 , is_bot :: Bool
+                 , from_first_name :: T.Text
+                 , from_username :: T.Text 
+                 , language_code :: T.Text
+                 } deriving Show
+
+instance FromJSON From where
+  parseJSON (Object v) =
+    From <$> v .: "id"
+         <*> v .: "is_bot" 
+         <*> v .: "first_name"
+         <*> v .: "username"
+         <*> v .: "language_code"                 
+
+data Message = Message {
+                         message_id :: Int
+                       , from :: From
+                       , chat :: Chat
+                       , date :: Int 
+                       , text :: T.Text
+                       } deriving (Show, Generic)
+instance FromJSON Message                          
+
+data Pastoral = Pastoral {
+                           update_id :: Int
+                         , message :: Message                      
+                         } deriving (Show, Generic)
+instance FromJSON Pastoral
+
+data WholeObject = WholeObject {
+                                  ok :: Bool
+                               ,  result :: [Pastoral] 
+                               } deriving (Show, Generic)
+instance FromJSON WholeObject                               
+
+objectFromJSON :: LC.ByteString -> Maybe WholeObject
+objectFromJSON a = decode a
 
 myToken :: String
-myToken = "xxx"
+myToken = Prelude.init $ unsafePerformIO $ getToken "../token.tg"
+
+getToken :: String -> IO String
+getToken fileName = readFile fileName
 
 messengerHost :: String
 messengerHost = "api.telegram.org/bot"
@@ -34,46 +95,7 @@ sendRequest = do
 main :: IO ()
 main = do
     x <- sendRequest
-    LC.putStrLn x
+    print $ objectFromJSON x
 
 
-{-
-{
-  "ok":true,
-  "result":[
-    {
-     "update_id":76765526,
-     "message":{ "message_id":8,
-                 "from":{ "id":195352543,
-                          "is_bot":false,
-                          "first_name":"\u0428\u0430\u043c\u0438\u043b\u044c",
-                          "username":"S_a_m_a_i_l",
-                          "language_code":"ru"},
-                          "chat":{ "id":195352543,
-                                   "first_name":"\u0428\u0430\u043c\u0438\u043b\u044c",
-                                   "username":"S_a_m_a_i_l",
-                                   "type":"private"
-                                 },
-                          "date":1612970774,
-                          "text":"Hello my friend!"
-               }
-    },
-    { "update_id":76765527,
-      "message":{ "message_id":9,
-                  "from":{ "id":195352543,
-                           "is_bot":false,
-                           "first_name":"\u0428\u0430\u043c\u0438\u043b\u044c",
-                           "username":"S_a_m_a_i_l",
-                           "language_code":"ru"
-                         },
-                  "chat":{ "id":195352543,
-                           "first_name":"\u0428\u0430\u043c\u0438\u043b\u044c",
-                           "username":"S_a_m_a_i_l",
-                           "type":"private"
-                         },
-                  "date":1612972530,
-                  "text":"Hi! Do you speak English?"
-                }
-    }]
-}
--}
+
