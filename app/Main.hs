3741b8ade3b78a903325d2697ab48f23b4c3410b
Shamil <ns-80@yandex.ru>
2021-02-16 21:57:39 +0500

    Add cycle

--- app/Main.hs
+++ app/Main.hs
@@ -1,51 +1,56 @@
-{-# LANGUAGE OverloadedStrings, DeriveGeneric #-} 
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import System.IO
-import System.IO.Unsafe (unsafePerformIO)
-import Control.Monad.Identity
+import Control.Monad.Identity ( Identity(runIdentity) )
+import Data.Aeson
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as BC
 import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString.Lazy.Char8 as LC
+import Data.Text as T hiding (last)
+import GHC.Generics ( Generic )
 import Network.HTTP.Simple
-import Data.Aeson
-import Data.Text as T hiding (last) 
-import GHC.Generics
+import System.IO
+import System.IO.Unsafe (unsafePerformIO)
 
+newtype Chat = Chat {id :: Int} deriving (Show, Generic)
 
-data Chat = Chat { id :: Int } deriving (Show, Generic)              
+data Message = Message
+  { message_id :: Int, 
+    chat :: Chat
+  }
+  deriving (Show, Generic)
 
-data Message = Message {
-                         message_id :: Int                  --    
-                       , chat :: Chat
-                       } deriving (Show, Generic)             
+data MessageDate = MessageDate
+  { update_id :: Int,
+    message :: Message
+  }
+  deriving (Show, Generic)
 
-data MessageDate = MessageDate {
-                                 update_id :: Int
-                               , message :: Message                      
-                               } deriving (Show, Generic)
+data WholeObject = WholeObject
+  { ok :: Bool,
+    result :: [MessageDate]
+  }
+  deriving (Show, Generic)
 
-data WholeObject = WholeObject {
-                                  ok :: Bool
-                               ,  result :: [MessageDate] 
-                               } deriving (Show, Generic)
+instance FromJSON Chat
 
-instance FromJSON Chat 
-instance FromJSON Message                                
-instance FromJSON MessageDate                               
-instance FromJSON WholeObject                               
+instance FromJSON Message
 
+instance FromJSON MessageDate
 
+instance FromJSON WholeObject
 
 objectFromJSON :: LC.ByteString -> Maybe WholeObject
-objectFromJSON a = decode a
+objectFromJSON = decode 
 
 myToken :: String
-myToken = Prelude.init $ unsafePerformIO $ getToken "../token.tg"
+myToken = getDataFromFile "../config.cfg" 1
 
-getToken :: String -> IO String
-getToken fileName = readFile fileName
+--getToken :: String -> IO String
+--getToken = readFile 
 
 messengerHost :: String
 messengerHost = "api.telegram.org/bot"
@@ -53,50 +58,77 @@ messengerHost = "api.telegram.org/bot"
 getUpdates :: String
 getUpdates = "/getUpdates"
 
-stringRequest :: String -> Request
-stringRequest str = parseRequest_ $ 
-  mconcat ["https://", messengerHost, myToken, str]
+lastUpdateID = read $ getDataFromFile "../config.cfg" 2 :: Int
 
-sendRequest :: IO LC.ByteString
-sendRequest = do
-    res <- httpLBS $ stringRequest getUpdates
-    if (getResponseStatusCode res == 200 )
-      then pure $ getResponseBody res
-      else pure "Error! Broken request!"
+getDataFromFile :: String -> Int -> String
+getDataFromFile fileName num =  
+  unsafePerformIO $ do
+    file <- openFile fileName ReadMode
+    firstLine <- hGetLine file    
+    secondLine <- hGetLine file    
+    hClose file
+    case num of
+     1 -> pure firstLine
+     _ -> pure secondLine
     
+  
+
+stringRequest :: String -> Request
+stringRequest str =
+  parseRequest_ $
+    mconcat ["https://", messengerHost, myToken, str]
+
+--sendRequest :: IO LC.ByteString
+--sendRequest = do
+  --res <- httpLBS $ stringRequest getUpdates
+  --if getResponseStatusCode res == 200
+    --then pure $ getResponseBody res
+    --else pure "Error! Broken request!"
+
 stringRepeat :: Maybe WholeObject -> String
-stringRepeat obj  = runIdentity $ do
-  let messageId = show $ message_id message' 
-  let chatId    = show $ Main.id $ chat message'
-  pure $ mconcat [
-                   "/copyMessage?chat_id=", chatId, 
-                   "&from_chat_id=",        chatId,
-                   "&message_id=",          messageId  
-                 ] 
-  where  
-    Just message' = message <$> last <$> result <$> obj                       
+stringRepeat obj = runIdentity $ do
+  let messageId = show $ message_id message'
+  let chatId = show $ Main.id $ chat message'
+  pure $
+    mconcat
+      [ "/copyMessage?chat_id=",
+        chatId,
+        "&from_chat_id=",
+        chatId,
+        "&message_id=",
+        messageId
+      ]
+  where
+    Just message' = message <$> last <$> result <$> obj
 
 sandRepeats :: Int -> String -> IO (Response LC.ByteString)
-sandRepeats n str  
+sandRepeats n str
   | n <= 1 = repeatRequest
   | otherwise = do
-                  repeatRequest
-                  sandRepeats (n-1) str 
+    repeatRequest
+    sandRepeats (n -1) str
   where
-    repeatRequest = httpLBS $ stringRequest $ str                    
-                            
-                                                  
+    repeatRequest = httpLBS $ stringRequest $ str
     
-
+getUpdateID :: Maybe WholeObject -> Int 
+getUpdateID obj = val 
+  where Just val = update_id <$> last <$> result <$> obj   
+    
 
 main :: IO ()
 main = do
-    x <- sendRequest
-    let obj = objectFromJSON x
-    print $ last <$> result <$> obj
-    sandRepeats 1 $ stringRepeat obj
-    print "Ok"
+  x <- httpLBS $ stringRequest getUpdates -- sendRequest
+  let obj = objectFromJSON $ getResponseBody x
+  case result <$> obj of
+    Just [] -> main
+    _       -> do
+      if getUpdateID obj == lastUpdateID
+        then main
+        else do  
+            print $ result <$> obj
+            sandRepeats 3 $ stringRepeat obj
+            print "Ok"
+   --print $ myToken
+   --print $ lastUpdateID             
+             
     
-
-
-
