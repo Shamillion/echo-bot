479da07f43eca43d30f927798950671253bde6ce
Shamil <ns-80@yandex.ru>
2021-02-22 22:08:09 +0500

    add ifKeyword

--- app/Main.hs
+++ app/Main.hs
@@ -3,14 +3,14 @@
 
 module Main where
 
-import Control.Monad.Identity ( Identity(runIdentity) )
+import Control.Monad.Identity (Identity (runIdentity))
 import Data.Aeson
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as BC
 import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString.Lazy.Char8 as LC
 import Data.Text as T hiding (last)
-import GHC.Generics ( Generic )
+import GHC.Generics (Generic)
 import Network.HTTP.Simple
 import System.IO
 import System.IO.Unsafe (unsafePerformIO)
@@ -18,39 +18,46 @@ import System.IO.Unsafe (unsafePerformIO)
 newtype Chat = Chat {id :: Int} deriving (Show, Generic)
 
 data Message = Message
-  { message_id :: Int, 
-    chat :: Chat
+  { message_id :: Int
+  , chat :: Chat
+  , text :: Maybe T.Text
   }
-  deriving (Show, Generic)
+  deriving (Show)
 
 data MessageDate = MessageDate
-  { update_id :: Int,
-    message :: Message
+  { update_id :: Int
+  , message :: Message
   }
   deriving (Show, Generic)
 
 data WholeObject = WholeObject
-  { ok :: Bool,
-    result :: [MessageDate]
+  { ok :: Bool
+  , result :: [MessageDate]
   }
   deriving (Show, Generic)
 
 newtype KeyboardButton = KeyboardButton
-  { text :: T.Text} deriving (Show, Generic) 
-  
-newtype KeyboardButtons = KeyboardButtons [KeyboardButton]  
-  deriving (Show, Generic) 
-  
+  {number :: T.Text}
+  deriving (Show, Generic)
+
+newtype KeyboardButtons = KeyboardButtons [KeyboardButton]
+  deriving (Show, Generic)
+
 data ReplyKeyboardMarkup = ReplyKeyboardMarkup
-  { keyboard :: [KeyboardButtons],
-    resize_keyboard :: Bool,
-    one_time_keyboard :: Bool
+  { keyboard :: [KeyboardButtons]
+  , resize_keyboard :: Bool
+  , one_time_keyboard :: Bool
   }
-  deriving (Show, Generic)  
+  deriving (Show, Generic)
 
 instance FromJSON Chat
 
-instance FromJSON Message
+instance FromJSON Message where
+  parseJSON (Object v) = do
+    message_id <- v .: "message_id"
+    chat <- v .: "chat"
+    text <- v .:? "text"
+    pure $ Message message_id chat text
 
 instance FromJSON MessageDate
 
@@ -63,7 +70,7 @@ instance ToJSON KeyboardButtons
 instance ToJSON ReplyKeyboardMarkup
 
 objectFromJSON :: LC.ByteString -> Maybe WholeObject
-objectFromJSON = decode 
+objectFromJSON = decode
 
 myToken :: String
 myToken = getDataFromFile "../config.cfg" 1
@@ -77,15 +84,15 @@ getUpdates num = mconcat ["/getUpdates?offset=", show num]
 lastUpdateID = read $ getDataFromFile "../config.cfg" 2 :: Int
 
 getDataFromFile :: String -> Int -> String
-getDataFromFile fileName num =  
+getDataFromFile fileName num =
   unsafePerformIO $ do
     file <- openFile fileName ReadMode
-    firstLine <- hGetLine file    
-    secondLine <- hGetLine file    
+    firstLine <- hGetLine file
+    secondLine <- hGetLine file
     hClose file
     case num of
-     1 -> pure firstLine
-     _ -> pure secondLine
+      1 -> pure firstLine
+      _ -> pure secondLine
 
 stringRequest :: String -> Request
 stringRequest str =
@@ -98,15 +105,15 @@ stringRepeat obj = runIdentity $ do
   let chatId = show $ Main.id $ chat message'
   pure $
     mconcat
-      [ "/copyMessage?chat_id=",
-        chatId,
-        "&from_chat_id=",
-        chatId,
-        "&message_id=",
-        messageId
+      [ "/copyMessage?chat_id="
+      , chatId
+      , "&from_chat_id="
+      , chatId
+      , "&message_id="
+      , messageId
       ]
-  where
-    Just message' = message <$> last <$> result <$> obj
+ where
+  Just message' = message <$> last <$> result <$> obj
 
 sandRepeats :: Int -> String -> IO (Response LC.ByteString)
 sandRepeats n str
@@ -114,79 +121,115 @@ sandRepeats n str
   | otherwise = do
     repeatRequest
     sandRepeats (n -1) str
-  where
-    repeatRequest = httpLBS $ stringRequest $ str
-    
-getUpdateID :: Maybe WholeObject -> Int 
-getUpdateID obj = val 
-  where Just val = update_id <$> last <$> result <$> obj   
+ where
+  repeatRequest = httpLBS $ stringRequest $ str
+
+getUpdateID :: Maybe WholeObject -> Int
+getUpdateID obj = val
+ where
+  Just val = update_id <$> last <$> result <$> obj
+
+ifKeyWord :: Maybe WholeObject -> IO ()
+ifKeyWord obj = case text message' of
+  Just "/repeat" -> print "It's necessary to send keybord."
+  Just "/help" -> print "It's necessary to send description."
+  _ -> print ()
+ where
+  Just message' = message <$> last <$> result <$> obj
 
 one :: KeyboardButton
-one = KeyboardButton 
-  {text = "1"}
-  
+one =
+  KeyboardButton
+    { number = "1"
+    }
+
 two :: KeyboardButton
-two = KeyboardButton 
-  {text = "2"}
-  
+two =
+  KeyboardButton
+    { number = "2"
+    }
+
 three :: KeyboardButton
-three = KeyboardButton 
-  {text = "3"}
-  
+three =
+  KeyboardButton
+    { number = "3"
+    }
+
 four :: KeyboardButton
-four = KeyboardButton 
-  {text = "4"}        
+four =
+  KeyboardButton
+    { number = "4"
+    }
 
 five :: KeyboardButton
-five = KeyboardButton 
-  {text = "5"}
+five =
+  KeyboardButton
+    { number = "5"
+    }
 
 buttons :: KeyboardButtons
 buttons = KeyboardButtons [one, two, three, four, five]
 
 numRepeat :: ReplyKeyboardMarkup
-numRepeat = ReplyKeyboardMarkup
-  { keyboard = [buttons],
-    resize_keyboard = True,
-    one_time_keyboard = True
-  }
-  
-sendKeybord :: String -> String
-sendKeybord str = Prelude.foldl fun "" str
-  where
-    fun acc c = 
-      acc ++ case c of
-                '{' -> "%7B"
-                '}' -> "%7D"
-                '[' -> "%5B" 
-                ']' -> "%5D"
-                '\"'-> "%22"
-                ' ' -> "+"
-                ',' -> "%2C"
-                ':' -> "%3A"
-                _   -> [c]
-                
-    
-            
+numRepeat =
+  ReplyKeyboardMarkup
+    { keyboard = [buttons]
+    , resize_keyboard = True
+    , one_time_keyboard = True
+    }
+
+question :: String
+question = "Currently set to 4 repetitions.\nHow many reps do you want to set?"
+
+stringToUrl :: String -> String
+stringToUrl str = Prelude.foldl fun "" str
+ where
+  fun acc c =
+    acc ++ case c of
+      '{' -> "%7B"
+      '}' -> "%7D"
+      '[' -> "%5B"
+      ']' -> "%5D"
+      '\"' -> "%22"
+      '\n' -> "%0A"
+      ' ' -> "%20"
+      ',' -> "%2C"
+      '.' -> "%2E"
+      ':' -> "%3A"
+      _ -> [c]
+
+sendKeyboard :: Maybe WholeObject -> IO (Response LC.ByteString)
+sendKeyboard obj =
+  httpLBS $
+    stringRequest $
+      mconcat
+        [ "/sendMessage?chat_id="
+        , show $ Main.id $ chat message'
+        , "&text="
+        , stringToUrl question
+        , "&reply_markup="
+        , stringToUrl $ LC.unpack $ encode numRepeat
+        ]
+ where
+  Just message' = message <$> last <$> result <$> obj
+
 endlessCycle :: Int -> IO ()
 endlessCycle updateID = do
-  x <- httpLBS $ stringRequest $ getUpdates updateID 
-  case getResponseStatusCode x == 200 of 
+  x <- httpLBS $ stringRequest $ getUpdates updateID
+  case getResponseStatusCode x == 200 of
     False -> print "Error! Broken request!"
-    _     -> do
+    _ -> do
       let obj = objectFromJSON $ getResponseBody x
       case result <$> obj of
         Just [] -> endlessCycle updateID
-        _       -> do
+        _ -> do
           let newUpdateID = 1 + getUpdateID obj
+          ifKeyWord obj
           print $ last <$> result <$> obj
           sandRepeats 3 $ stringRepeat obj
-          endlessCycle newUpdateID           
-                
-           
-main :: IO ()
-main = do 
-  --endlessCycle 0 
-  httpLBS $ stringRequest $ "/sendMessage?chat_id=195352543&text=Currently%20set%20to%204%20repetitions&reply_markup=" ++ (sendKeybord $ LC.unpack $ encode numRepeat)
-  print "OK"
+          -- sendKeyboard obj
+          endlessCycle newUpdateID
 
+main :: IO ()
+main = do
+  endlessCycle 0
