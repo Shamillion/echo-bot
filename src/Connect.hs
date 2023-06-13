28c088e49ff48cfd4e59d9878e185c01d886b8b7
Shamil <ns-80@yandex.ru>
2023-06-13 23:00:26 +0500

    Change Connect.hs #2

--- src/Connect.hs
+++ src/Connect.hs
@@ -7,12 +7,16 @@ import qualified Data.ByteString.Lazy.Char8 as LC
 import Data.Time (getCurrentTime)
 import Logger.Data (Priority (DEBUG, ERROR))
 import Logger.Functions (writingLine)
+import Network.HTTP.Conduit
+  ( HttpExceptionContent (ConnectionFailure),
+  )
 import Network.HTTP.Simple
-  ( HttpException,
+  ( HttpException (HttpExceptionRequest),
     Request,
     Response,
     httpLBS,
   )
+import System.Exit (die)
 
 -- Function for connecting to the server.
 connectToServer :: Request -> Int -> IO (Response LC.ByteString)
@@ -20,14 +24,18 @@ connectToServer req num = do
   x <- try $ httpLBS req
   writingLine DEBUG $ show req
   case x of
-    Left e -> do
-      writingLine ERROR $ show (e :: HttpException)
+    Left cf@(HttpExceptionRequest _ (ConnectionFailure _)) -> do
+      writingLine ERROR $ show (cf :: HttpException)
       when (num == 0) $ do
         getCurrentTime >>= print
         putStrLn "Connection Failure"
-        putStrLn "Trying to set a connection... "
+      putStrLn "Trying to set a connection... "
       threadDelay 1000000
       connectToServer req (num + 1)
+    Left e -> do
+      let err = show (e :: HttpException)
+      writingLine ERROR err
+      die err
     Right v -> do
       writingLine DEBUG $ show v
       when (num /= 0) $ do
